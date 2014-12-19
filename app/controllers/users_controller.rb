1d4617eeb88c8727a638f64d718a4df8797f3ca6
Mashrur Hossain <mashrur.hossain@gmail.com>
2014-12-19 00:03:51 +0000

    update layout, add navbar and display users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
+  def new
+  
+  end
+  
+  def create
+  
+  end
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
 
   root to: 'static_pages#home'
+  
+  resources :users
 end
