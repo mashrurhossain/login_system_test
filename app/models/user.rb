6176cd68092a663c89df356914d067b0165240d6
Mashrur Hossain <mashrur.hossain@gmail.com>
2014-12-19 06:16:24 +0000

    make authentication system for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   has_secure_password validations: false
+  
+  validates :password, presence: true, on: :create, length: { minimum: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
   
-  resources :users
+  resources :users, except: [:new]
+  
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
 end
