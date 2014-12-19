4d266912343837c9f09c8c84f21bdf6123442e79
Mashrur Hossain <mashrur.hossain@gmail.com>
2014-12-19 00:19:08 +0000

    add password digest and model updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+  has_secure_password validations: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get '/home', to: 'static_pages#home'
 
-  root to: 'static_pages#home'
+  root to: 'users#index'
   
   resources :users
 end
